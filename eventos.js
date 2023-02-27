ac4d5e112d689e317a945ab189ac8397502b432f
patricia pitzus <patriciapitzus@gmail.com>
2023-02-27 00:08:25 -0300

    trabajofinal

---
+++ eventos.js
@@ -0,0 +1,10 @@
+
+/**document.addEventListener('click', function() {
+    console.log("mensaje de trabajo");
+    document.getElementById("formulario").innerHTML= "Tu mensaje ha sido enviado. Gracias"
+})*/
+
+
+function test(){
+    alert('Gracias por comunicarte!');
+};
